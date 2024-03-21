cd9aeb4b6d8fee7e42efe72800a9e0b03ebab56b
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-03-21 10:16:56 +0000

    done

--- Script.js
+++ Script.js
@@ -6,4 +6,18 @@ return value.json();
 
 }).then((ct)=>{
 console.log(ct)
+let crptdata=""
+ct.data.map((crp)=>{
+    crptdata+=`<div class="crptraw">
+    <div><h2>${crp.rank}</h2></div>
+    <div><h2>${crp.name} ${crp.symbol}</h2></div>
+    <div><h2>${crp.changePercent24Hr}%</h2>
+    </div>
+    <div><h2>₹${crp.priceUsd}</h2>
+    </div>
+    <div><button><a href="${crp.explorer}" target="_blank"> Track </a></button></div>
+    </div>`
+})
+document.getElementById("datacrp").innerHTML=crptdata;
+
 });
