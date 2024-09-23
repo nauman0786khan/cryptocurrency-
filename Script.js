ffa516299f09ceee20f256909438e410e1580f02
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-09-23 10:17:07 +0000

    Done crop

--- Script.js
+++ Script.js
@@ -20,24 +20,19 @@ ct.data.forEach((crp)=>{
 document.getElementById("datacrp").innerHTML=crptdata;
 });
 
-// let p = fetch("https://api.coincap.io/v2/assets");
-// p.then((value) => {
-//     return value.json();
-// }).then((ct) => {
-//     console.log(ct);
-//     let crptdata = "";
-//     ct.data.forEach((crp) => {
-//         // Applying toFixed() to format priceUsd with 2 decimal places
-//         let formattedPrice = parseFloat(crp.priceUsd).toFixed(2);
-//         // Applying toFixed() to format changePercent24Hr with 2 decimal places
-//         let formattedPercentage = parseFloat(crp.changePercent24Hr).toFixed(2);
-//         crptdata += `<div class="crptraw">
-//             <div><h2>${crp.rank}</h2></div>
-//             <div><h2>${crp.name} ${crp.symbol}</h2></div>
-//             <div><h2 class="per">${formattedPercentage}%</h2></div>
-//             <div><h2 class="pp">₹${formattedPrice} USD</h2></div>
-//             <div><button><a href="${crp.explorer}" target="_blank">Track</a></button></div>
-//         </div>`;
-//     });
-//     document.getElementById("datacrp").innerHTML = crptdata;
-// });
+$(".scan").on("keyup", function() {
+    var ff1 = $(this).val().toLowerCase();
+    
+    if(ff1 === "") {
+      // Show all rows if input is empty
+      $(".crptraw").show();
+    } else {
+      // Filter the rows based on search input
+      $("#datacrp .crptraw").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(ff1) > -1);
+      });
+    }
+  });
+
+
+  
